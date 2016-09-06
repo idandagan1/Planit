eb43e5896c23f6fd8edf92d78a6b5e3813053c10
idandagan1 <idandagan1@gmail.com>
2016-09-06 19:04:17 +0300

    commiting

--- app.js
+++ app.js
@@ -14,8 +14,9 @@ var app = express();
 
 // view engine setup
 //app.set('port', process.env.PORT || 3000);
+app.engine('html', require('ejs').renderFile);
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'html');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
--- routes/index.js
+++ routes/index.js
@@ -2,17 +2,27 @@ var express = require('express');
 var router = express.Router();
 var mongo = require('mongodb').MongoClient;
 var assert = require('assert');
-
+var resultArray = [];
 var url = 'mongodb://localhost:27017/local';
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
 
-  res.render('index');
+  res.render('map');
+});
+
+router.get('/list', function(req, res, next) {
+
+  res.render('list');
+  res.redirect('/');
+});
+router.get('/index', function(req, res, next) {
+
+  res.redirect('/index');
 });
 
 router.get('/get-data', function(req,res,next){
-  var resultArray = [];
+
   mongo.connect(url,function(err,db){
     assert.equal(null,err);
     var cursor = db.collection('user-data').find();
@@ -35,17 +45,24 @@ router.post('/insert', function(req,res,next){
     cost: req.body.cost
   };
 
+  resultArray.push(item);
   mongo.connect(url, function(err, db){
     assert.equal(null,err);
     db.collection('user-data').insertOne(item,function(err,result){
       assert.equal(null,err);
       console.log("Item has been insert! You are the best!!");
+      onAddItem(item);
       db.close();
     })
   })
 
   res.redirect('/');
 })
+
+function onAddItem(newItem){
+  var a=9;
+
+};
 
 
 module.exports = router;
