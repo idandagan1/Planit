dcc8217fcb4ae7e083ed81e4e01311318977913d
idandagan1 <idandagan1@gmail.com>
2017-04-20 00:14:20 +0300

    Converting to ES6

--- app.js
+++ app.js
@@ -1,39 +1,42 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-var session = require('express-session');
-
-var mongoose = require('mongoose');
+import express from 'express';
+import logger from 'morgan';
+import cookieParser from 'cookie-parser';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import session from 'express-session';
 mongoose.connect('localhost:27017/local');
-var Schema = mongoose.Schema;
-var app = express();
-var port = process.env.PORT || 3000;
 
-console.log("Server listening on port " + port);
-
-var routes = require('./routes/index');
-var map = require('./routes/map');
-var list = require('./routes/list');
+const app = express();
+const port = process.env.PORT || 3000;
+const routes = require('./routes/index');
+const map = require('./routes/map');
+const list = require('./routes/list');
 
 app.locals.currentUser = null;
 app.locals.validator = require('validator');
+
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        httpOnly: false,
+    },
+}));
 
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-//app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/'));
 
-//app.use('/users', routes);
 app.use('/', routes);
 app.use('/list.html', list);
 app.use('/map.html', map);
 
 module.exports = app;
-app.listen(port);
+app.listen(port, () => {
+    console.log(`Server listening on port ${port}`);
+});
 
-
+/*./mongod --bind_ip localhost*/
